86a28adff23094c4ac13d7203b73d5464f1bb9d2
AnafaHyams <anafa18@gmail.com>
2021-05-22 19:11:07 +0300

    Gender and MaritalStatus enums

---
+++ src/main/java/utils/enums/Gender.scala
@@ -0,0 +1,6 @@
+package utils.enums
+
+object Gender extends Enumeration {
+  type Gender = Value
+  val Female, Male = Value
+}
---
+++ src/main/java/utils/enums/MaritalStatus.scala
@@ -0,0 +1,6 @@
+package utils.enums
+
+object MaritalStatus extends Enumeration{
+  type MaritalStatus = Value
+  val Single, Married = Value
+}
