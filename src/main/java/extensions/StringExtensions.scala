b53309f9db176a0b4b560c453bb54edc7510274d
AnafaHyams <anafa18@gmail.com>
2021-05-28 00:12:20 +0300

    String extensions in given format instead of functions and checks

--- src/main/java/extensions/StringExtensions.scala
+++ src/main/java/extensions/StringExtensions.scala
@@ -1,34 +1,15 @@
 package extensions
 
-import extensions.Registry.getNewCharExtensions
-
 class StringExtensions(str: String) {
 
   def isValidPhoneNumber(): Boolean = {
-    var stringIsValidPhoneNumber: Boolean = true
-
     val validPhoneFormat = """^(\d{3}[-])(\d{4}[-])\d{2}$|^(\+\d{1,3}( )?)?((\(\d{3}\))|\d{3})[- .]?\d{3}[- .]?\d{4}$"""
     str.matches(validPhoneFormat)
-
-    /*str.foreach(char => {
-      val charIsValidPhoneNumber = char.isDigit || char.isValidCharForPhoneNumber()
-      stringIsValidPhoneNumber = stringIsValidPhoneNumber && charIsValidPhoneNumber
-    })*/
-
-    //stringIsValidPhoneNumber
   }
 
   def isValidEmail(): Boolean = {
-    var stringIsValidEmail: Boolean = false
-
     val validEmailFormat = """^[a-zA-Z0-9\.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$"""
     str.matches(validEmailFormat)
-
-    /*if(str.endsWith(".com") && str.contains("@")) {
-      stringIsValidEmail = true
-    }
-
-    stringIsValidEmail*/
   }
 
 }
