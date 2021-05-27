f7e9d0bd6fda2ab1dbb33cbee19103ddb77973a7
AnafaHyams <anafa18@gmail.com>
2021-05-27 22:39:04 +0300

    cleaning start file

--- src/main/java/Start.scala
+++ src/main/java/Start.scala
@@ -1,5 +1,4 @@
 import extensions.CSVHandlers.StringExt
-import extensions.Registry.{getNewIntExtensions, getNewStringExtensions}
 import extensions.ListExtensions.{ClientListExtensions, PersonListExtensions}
 import extensions.ObjectTypeChangingExtensions.{SwitchFromClientToUserObject, SwitchFromPersonToUserObject}
 import models.{Client, Person, User}
@@ -10,46 +9,17 @@ import scala.io.Source
 object Start {
 
   def main(args: Array[String]): Unit = {
-    /*
-    println("anafa.hyams@gmail.com".isValidEmail())
-    println("anafa.hyams@gmail.com3".isValidEmail())
-    println("anafa.hyamsgmail.com".isValidEmail())
-    println("anafa.hyams@gmail".isValidEmail())
-
-    println("---------------------------------")
-
-    println("anafa.hyams@gmail".isValidPhoneNumber())
-    println("+1 (997) 411-3097".isValidPhoneNumber())
-    println("+1 ?997) 411-3097".isValidPhoneNumber())
-    println("+1 (?)997) 411-309a".isValidPhoneNumber())
-
-    println("---------------------------------")
-
-    val negativeAge: Int = -1
-    println(1.isValidAge())
-    println(negativeAge.isValidAge())
-
-    println("*********************************************************")
-*/
 
   val clientsFileName = DataLocationReader.clientFileName
   val personsFileName = DataLocationReader.personFileName
   val requestFileName = DataLocationReader.requestFileName
 
-    val clientsListFromExcel: List[Client] = ReadDataFromExcel.readData(clientsFileName)
-    val personsListFromJson: List[Person] = ReadDataFromJson.readData(personsFileName)
+  val clientsListFromExcel: List[Client] = ReadDataFromExcel.readData(clientsFileName)
+  val personsListFromJson: List[Person] = ReadDataFromJson.readData(personsFileName)
 
-/*
-    //PersonsList
-    val personsCSV = Source.fromFile("data/persons.csv").getLines().toList
-    val personsList = personsCSV.map(person => person.getPerson(","))
-    personsList.foreach(person => println(person))
-*/
-
-      // Request
+    // Request
     val requestCSV = Source.fromFile(requestFileName).getLines().toList
     val requestList = requestCSV.map(request => request.getRequest(","))
-    //requestList.foreach(person => println(person))
     val request = requestList.head
 
     // valid persons and clients list
@@ -59,8 +29,6 @@ object Start {
     // creating users list from persons and clients list
     val personsUsersList: List[User] = validPersonsList.map(person => SwitchFromPersonToUserObject(person))
     val clientsUsersList: List[User] = validClientsList.map(client => SwitchFromClientToUserObject(client))
-
-    //var userList: List[User] = Nil
     val userList: List[User] = personsUsersList ::: clientsUsersList
 
     // Task 3: Filter by age minimum value, maximum value in two tables
