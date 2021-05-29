57ff2f05b84b024915735c8ca6fc1c6ee47d6892
AnafaHyams <anafa18@gmail.com>
2021-05-29 19:48:06 +0300

    Printing results to screen

--- src/main/java/flow/FlowManager.scala
+++ src/main/java/flow/FlowManager.scala
@@ -7,6 +7,7 @@ import services.{DataLocationReader, ReadDataFromExcel, ReadDataFromJson}
 
 
 object FlowManager {
+
   def usersFlowManager(): Unit = {
     val clientsFileName = DataLocationReader.clientFileName
     val personsFileName = DataLocationReader.personFileName
@@ -30,11 +31,17 @@ object FlowManager {
     println("Task 3:")
     println("Filter by age minimum value, maximum value in two tables")
     println(s"There are ${listOfUsersWithAgeBetween.length} users with age between ${request.minAge} to ${request.maxAge}")
+    printFilteredUserList(listOfUsersWithAgeBetween)
+
+    println("---------------------------------------------------------------------------------------")
 
     println("Task 4:")
     val listOfUsersStartsWithePrefix: List[User] = userList.filter(user => user.firstName.startsWith(request.prefixName))
     println("Get all people whose names start with a certain prefix. As like in request.json")
     println(s"There are ${listOfUsersStartsWithePrefix.length} users with Prefix name ${request.prefixName}")
+    printFilteredUserList(listOfUsersStartsWithePrefix)
+
+    println("---------------------------------------------------------------------------------------")
 
     println("Task 5:")
     println("From the client table, get all married men who have more than 2 children. Search by fields: marital status, gender and number of Children")
@@ -42,9 +49,26 @@ object FlowManager {
       .filter(client => client.gender.toLowerCase == request.gender)
       .filter(client => client.numberOfChildren == request.numberOfChildren)
     println(s"There are ${clientMarriedMenWith2ChildrenList.length} ${request.maritalStatus} ${request.gender} clients with ${request.numberOfChildren} children")
+    clientMarriedMenWith2ChildrenList.foreach(client => println(client))
 
-    val filteredUsersListByRequest: List[User] = userList.filter(user => user.filterByRequest(request))
-    //val clientsListBy_MS_G_NOC: List[Client] = validClientsList.filter(client => client.filterByRequest(request)) // last task
+    println("---------------------------------------------------------------------------------------")
 
+    //println("Filter By Request:")
+    //val filteredUsersListByRequest: List[User] = userList.filter(user => user.filterByRequest(request))
+    //println(s"There are ${filteredUsersListByRequest.length} ${request.maritalStatus} ${request.gender} clients between age ${request.minAge} to ${request.maxAge} with ${request.numberOfChildren} children that there name start with ${request.prefixName}")
+
+  }
+
+  def printFilteredUserList(filteredList: List[User]): Unit = {
+    filteredList.foreach(user => {
+      user match {
+        case client: SwitchFromClientToUserObject => //Client
+          print(s"Client(${client.firstName},${client.lastName},")
+        case person: SwitchFromPersonToUserObject => // Person
+          print(s"Person(${person.firstName} ${person.lastName}")
+      }
+
+      println(s"${user.gender},${user.age},${user.email},${user.phoneNumber})")
+    })
   }
 }
