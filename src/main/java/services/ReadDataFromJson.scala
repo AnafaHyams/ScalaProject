2614085fb6b10b89da69396fff6601e3551a971d
AnafaHyams <anafa18@gmail.com>
2021-05-29 19:06:51 +0300

    ReadDataFromJson works with json file instead of csv file: Removing all code that handle CSV files

--- src/main/java/services/ReadDataFromJson.scala
+++ src/main/java/services/ReadDataFromJson.scala
@@ -3,12 +3,10 @@ package services
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import extensions.CSVHandlers.StringExt
 import models.{Person, Request}
 
 import java.io.File
 import scala.collection.JavaConverters.{asJavaIterableConverter, iterableAsScalaIterableConverter}
-import scala.io.Source
 
 object ReadDataFromJson {
 
@@ -17,12 +15,7 @@ object ReadDataFromJson {
 
   def readData(personsFileName: String): List[Person] = {
     val jsonFile = new File(personsFileName)
-
     val personsList = mapper.readValue(jsonFile, new TypeReference[List[Person]]() {}).asJava.asScala.toList
-
-    // JUST FOR NOW: READ Persons data from csv file and return persons list
-    //val personsCSV = Source.fromFile(personsFileName).getLines().toList
-    //val personsList = personsCSV.map(person => person.getPerson(","))
 
     personsList
   }
